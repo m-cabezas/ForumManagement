231e0ffad3523390f8498614ba2935c0fe6d1a22
caybe <cabezasmathias@gmail.com>
2019-11-03 18:55:43 +0100

    - Méthodes "Show" du main - Début du header

--- src/controller/HeaderController.java
+++ src/controller/HeaderController.java
@@ -1,13 +1,44 @@
 package controller;
 
 import dao.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
 
 public class HeaderController {
+
+	@FXML
+	private ComboBox<String> userComboBox;
+	@FXML
+	private Text currentUserTxt;
 
 	private UserDAO userDAO;
 	private MainApp mainApp;
 
+	public HeaderController() {
+		userDAO = new UserDAO();
+	}
+
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
+	}
+
+	@FXML
+	public void initialize(){
+		if(mainApp.getUser() == null){
+			currentUserTxt.setVisible(false);
+		}else{
+			currentUserTxt.setText(mainApp.getUser().getPseudo());
+			currentUserTxt.setVisible(true);
+		}
+		// TODO: Populate the userComboBox thanks to the userDao
+
+	}
+
+	@FXML
+	public void pickUser(){
+
+		userComboBox.getSelectionModel().getSelectedItem();
+		// TODO: Set the mainApp user -> ex : mainApp.setUser(user)
 	}
 }
--- src/controller/MainApp.java
+++ src/controller/MainApp.java
@@ -1,14 +1,21 @@
 package controller;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.User;
 
-import static javafx.application.Application.launch;
 
-public class MainApp {
+import java.io.IOException;
+
+public class MainApp extends Application {
 
 	private Stage primaryStage;
 	private AnchorPane mainPane;
+	private User user;
 
 	private MessageListController messageListController;
 	private PostListController postListController;
@@ -17,43 +24,162 @@ public class MainApp {
 	private TopicListController topicListController;
 	private AdminController adminController;
 
-	public void showMainView() {
-		// TODO - implement MainApp.showMainView
-		throw new UnsupportedOperationException();
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		this.primaryStage = primaryStage;
+		primaryStage.setTitle("Forum Management");
+
+		initMainView();
+
+		showHeaderPane();
+		showTopicListPane();
+
+	}
+
+	private void initMainView() {
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("../view/MainView.fxml"));
+			mainPane = (AnchorPane) loader.load();
+			Scene scene = new Scene(mainPane);
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
 	}
 
 	public void showPostListPane() {
-		// TODO - implement MainApp.showPostListPane
-		throw new UnsupportedOperationException();
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("../view/PostListPane.fxml"));
+			AnchorPane postListPane = (AnchorPane) loader.load();
+			// Getting the BorderPane of the MainView
+			BorderPane mainBorderPane = (BorderPane) mainPane.getChildren().get(0);
+
+			//Adding pane to the center of the borderPane
+			mainBorderPane.setCenter(postListPane);
+
+			//Allowing Controller to access the view
+			postListController = loader.getController();
+			postListController.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showHeaderPane() {
-		// TODO - implement MainApp.showHeaderPane
-		throw new UnsupportedOperationException();
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("../view/HeaderPane.fxml"));
+			AnchorPane headerPane = (AnchorPane) loader.load();
+			// Getting the BorderPane of the MainView
+			BorderPane mainBorderPane = (BorderPane) mainPane.getChildren().get(0);
+
+			//Adding pane to the center of the borderPane
+			mainBorderPane.setTop(headerPane);
+
+			//Allowing Controller to access the view
+			headerController = loader.getController();
+			headerController.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showTopicListPane() {
-		// TODO - implement MainApp.showTopicListPane
-		throw new UnsupportedOperationException();
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("../view/TopicListPane.fxml"));
+			AnchorPane topicListPane = (AnchorPane) loader.load();
+			// Getting the BorderPane of the MainView
+			BorderPane mainBorderPane = (BorderPane) mainPane.getChildren().get(0);
+
+			//Adding pane to the center of the borderPane
+			mainBorderPane.setCenter(topicListPane);
+
+			//Allowing Controller to access the view
+			topicListController = loader.getController();
+			topicListController.setMainApp(this);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showAdminPane() {
-		// TODO - implement MainApp.showAdminPane
-		throw new UnsupportedOperationException();
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("../view/AdminPane.fxml"));
+			AnchorPane adminPane = (AnchorPane) loader.load();
+			// Getting the BorderPane of the MainView
+			BorderPane mainBorderPane = (BorderPane) mainPane.getChildren().get(0);
+
+			//Adding pane to the center of the borderPane
+			mainBorderPane.setCenter(adminPane);
+
+			//Allowing Controller to access the view
+			adminController = loader.getController();
+			adminController.setMainApp(this);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showMessageListPane() {
-		// TODO - implement MainApp.showMessageListPane
-		throw new UnsupportedOperationException();
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("../view/MessageListPane.fxml"));
+			AnchorPane messageListPane = (AnchorPane) loader.load();
+			// Getting the BorderPane of the MainView
+			BorderPane mainBorderPane = (BorderPane) mainPane.getChildren().get(0);
+
+			//Adding pane to the center of the borderPane
+			mainBorderPane.setCenter(messageListPane);
+
+			//Allowing Controller to access the view
+			messageListController = loader.getController();
+			messageListController.setMainApp(this);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void showUserPane() {
-		// TODO - implement MainApp.showUserPane
-		throw new UnsupportedOperationException();
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("../view/UserPane.fxml"));
+			AnchorPane userPane = (AnchorPane) loader.load();
+			// Getting the BorderPane of the MainView
+			BorderPane mainBorderPane = (BorderPane) mainPane.getChildren().get(0);
+
+			//Adding pane to the center of the borderPane
+			mainBorderPane.setCenter(userPane);
+
+			//Allowing Controller to access the view
+			userController = loader.getController();
+			userController.setMainApp(this);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
-		launch(args);
+			launch(args);
 	}
 
 }
