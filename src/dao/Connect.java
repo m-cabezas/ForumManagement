b95c0a916c71bdd1df19ac539b54764a7a813c00
Nao8617 <bKcwWXQZ6bFq>
2019-11-16 18:36:33 +0100

    La classe connect sah quel plaisir

--- src/dao/Connect.java
+++ src/dao/Connect.java
@@ -1,17 +1,23 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connect {
 
-	private String url;
-	private String user;
-	private String password;
-	private Connection connect;
+    private String url;
+    private String user;
+    private String password;
+    private Connection conn;
 
-	public Connection getInstance() {
-		// TODO - implement Connect.getInstance
-		throw new UnsupportedOperationException();
-	}
+    public Connection getInstance(){
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 
 }
