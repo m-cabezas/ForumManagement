d27ff1a51d55b1c0d11ea4e066a361c31617c067
Nao <mael.bourdon@outlook.fr>
2019-11-17 16:45:40 +0100

    User controller adding

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -3,6 +3,7 @@ package controller;
 import dao.*;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
+import model.User;
 
 public class UserController {
 
@@ -18,7 +19,30 @@ public class UserController {
 	private UserDAO userDAO;
 	private MainApp mainApp;
 
+	private User user;
+
+	public UserController() {
+		userDAO = new UserDAO();
+	}
+
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@FXML
+	public void initialize(){
+		if(user != null){
+			pseudoTxt.setText(user.getPseudo());
+			nameTxt.setText(user.getName());
+			surnameTxt.setText(user.getSurname());
+			ageTxt.setText(String.valueOf(user.getAge()));
+			biographyTxt.setText(user.getBiography());
+			userIdTxt.setText(String.valueOf(user.getId()));
+			adminTxt.setText(String.valueOf(user.isAdmin()));
+		}
 	}
 }
