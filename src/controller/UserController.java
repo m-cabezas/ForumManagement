4318ee1054c1513a0de8bbd40769b2761fddfa11
Nao8617 <bKcwWXQZ6bFq>
2019-11-17 03:17:35 +0100

    UserPane set, UserController FXML var set

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,8 +1,19 @@
 package controller;
 
 import dao.*;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class UserController {
+
+	@FXML
+    private Text pseudoTxt;
+	private Text nameTxt;
+	private Text surnameTxt;
+	private Text ageTxt;
+	private Text biographyTxt;
+	private Text userIdTxt;
+	private Text adminTxt;
 
 	private UserDAO userDAO;
 	private MainApp mainApp;
